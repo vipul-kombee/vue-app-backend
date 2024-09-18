f22e1d4fad157349e4e7974d985ee4dcc11a0cd8
unknown <patilnirav70@gmail.com>
2024-09-18 14:07:24 +0530

    initial commit

---
+++ Apis/User/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = mongoose.Schema({
+      type: {
+        type: String,
+        enum: ['BUYER', 'SELLER', 'SUPERADMIN'],
+        default: 'BUYER'
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Products'
+      }],
+      resetToken: {
+        type: String,
+      },
+      resetTokenExpiration: {
+        type: String
+      }
+})
+
+module.exports = mongoose.model('users', UserSchema);
